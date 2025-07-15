64fb6e6d26e2dbe9244169c9f4a585c4d41d8078
DKMFzF <flim.win@yandex.ru>
2025-07-16 02:57:08 +0700

    feat: added linter and formater

--- Makefile
+++ Makefile
@@ -1,9 +1,19 @@
 
-.PHONY: build
+.PHONY: start lint format
 
-build:
+start:
 	@echo "[MAKEFILE] Start project..."
 	chmod +x ./bin/start.sh
 	./bin/start.sh src/main.cpp || exit 1
 	@echo "[MAKEFILE] Finsh project"
+
+lint:
+	@echo "[MAKEFILE] Start Lint code..."
+	find src -name "*.cpp" -o -name "*.h" | xargs clang-format -i
+	@echo "[MAKEFILE] Finish Lint code"
+
+format:
+	@echo "[MAKEFILE] Start Format code"
+	find src -name "*.cpp" -o -name "*.h" | xargs clang-format --dry-run --Werror
+	@echo "[MAKEFILE] Finish Format code"
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,30 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-int main()
-{
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
-    }
+int main() {
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW" << std::endl;
+    return -1;
+  }
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Render Engine", nullptr, nullptr);
-    if (!window) {
-        std::cerr << "Failed to create window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-
-    while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
+  GLFWwindow *window =
+      glfwCreateWindow(800, 600, "Render Engine", nullptr, nullptr);
+  if (!window) {
+    std::cerr << "Failed to create window" << std::endl;
     glfwTerminate();
-    return 0;
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
 }
